cba20cc79f51c4a5fce221e15034effeac2797ae
Naima Malik <noma_m@hotmail.com>
2023-11-08 10:55:03 +0100

    Create dropdown and fetch different endpoints

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -5,10 +5,13 @@ import LazyImage from "../LazyImage/LazyImage";
 import Images from "../Images/Images";
 import { Link } from "react-router-dom";
 
+//Components
+import Dropdown from "../Dropdown/Dropdown";
 
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [selected, setSelected] = useState("");
 
   const apiKey = "e7d445a3d3b2d973d65584a1210ec5df";
 
@@ -18,14 +21,59 @@ const MovieList = () => {
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        console.log(data.results);
         setMovies(data.results);
         setLoading(false);
       });
   }, []);
 
+  const fetchMovies = (url, setData) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.results);
+      })
+      .catch((err) => console.log("Error fetching data", err));
+  };
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setSelected(value);
+
+    switch (value) {
+      case "popular":
+        fetchMovies(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`,
+          setMovies
+        );
+        break;
+      case "topRated":
+        fetchMovies(
+          `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`,
+          setMovies
+        );
+        break;
+      case "upcoming":
+        fetchMovies(
+          `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&primary_release_year=2024&page=1`,
+          setMovies
+        );
+
+        break;
+      case "newReleases":
+        fetchMovies(
+          `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`,
+          setMovies
+        );
+        break;
+      default:
+    }
+  };
+
   return (
     <>
+      <Dropdown value={selected} onChange={handleChange} />
+
       {loading ? (
         <div className="loading-spinner">
           <FadeLoader loading={loading} size={150} />{" "}
@@ -36,11 +84,9 @@ const MovieList = () => {
           return (
             <div key={index}>
               <p>{data.title}</p>
- <Link to={`/movieList/${data.id}`}>
-              <LazyImage
-                backdropPath={data.backdrop_path}
-              />
-               </Link>
+              <Link to={`/movieList/${data.id}`}>
+                <LazyImage backdropPath={data.backdrop_path} />
+              </Link>
             </div>
           );
         })}
