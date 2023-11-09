b80eea10bbda9276db12ab085fdc56b1553193a2
Naima Malik <noma_m@hotmail.com>
2023-11-09 18:26:20 +0100

    Handle error in case not movieId and no images available. Show PageNotFound

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -2,14 +2,21 @@ import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import BackButton from "../BackButton/BackButton";
 import "./MovieDetails.css";
+import PageNotFound from "../../components/PageNotFound/PageNotFound";
 
 const MovieDetails = () => {
   const { movieId } = useParams();
   const [movie, setMovie] = useState();
+  const [notFound, setNotFound] = useState(false);
 
   const apiKey = "e7d445a3d3b2d973d65584a1210ec5df";
 
   useEffect(() => {
+    if (isNaN(movieId)) {
+      setNotFound(true);
+      return;
+    }
+
     fetch(
       `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=en-US`
     )
@@ -17,13 +24,15 @@ const MovieDetails = () => {
       .then((data) => {
         console.log("Movie Details", data);
         setMovie(data);
+      })
+      .catch((error) => {
+        console.log("Error fetching movie details:", error);
+        setNotFound(true);
       });
   }, [movieId]);
 
-  //NOTE FOR MERGING!! I had to change the "movie &&" under the return to be able to do the
-  //destructuring, so I had to add this here instead. It should do the same thing though
-  if (!movie) {
-    return null;
+  if (!movie || notFound) {
+    return <PageNotFound />;
   }
 
   //Destructuring of movie data
@@ -38,7 +47,9 @@ const MovieDetails = () => {
     poster_path,
   } = movie;
 
-  const bgImg = `https://image.tmdb.org/t/p/original/${backdrop_path}`;
+  const bgImg = backdrop_path
+    ? `https://image.tmdb.org/t/p/original/${backdrop_path}`
+    : null;
 
   //Function to display runtime in hours and minutes
   const toHoursAndMinutes = (runtime) => {
@@ -57,14 +68,17 @@ const MovieDetails = () => {
         <div className="movie-content">
           <img
             className="cover-img"
-            src={`https://image.tmdb.org/t/p/w342${poster_path}`}
+            src={
+              poster_path
+                ? `https://image.tmdb.org/t/p/w342${poster_path}`
+                : null
+            }
           />
           {vote_average ? (
             <div className="rating-container">
               <h2 className="rating">⭐️ {vote_average.toFixed(1)}</h2>
             </div>
-          ) : null
-          }
+          ) : null}
           <div className="movie-info">
             <h1>{title}</h1>
             {genres && (
@@ -84,7 +98,11 @@ const MovieDetails = () => {
               </p>
             )}
 
-            {runtime ? (<p>Playtime: {convertMinutes.hours} h {convertMinutes.minutes} min</p>) : null}
+            {runtime ? (
+              <p>
+                Playtime: {convertMinutes.hours} h {convertMinutes.minutes} min
+              </p>
+            ) : null}
             <p>{overview}</p>
             {production_companies && (
               <div className="company">
